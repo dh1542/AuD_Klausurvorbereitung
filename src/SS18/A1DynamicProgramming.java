45c5b0c4d0525dcd8d4227203a91c09f9e4e0f0b
Dominik Fedora Desktop <dominik151099@outlook.de>
2022-11-26 14:10:30 +0100

    Implemented solution for SS18 A1b (Bottom up DP)

--- src/SS18/A1DynamicProgramming.java
+++ src/SS18/A1DynamicProgramming.java
@@ -33,8 +33,8 @@ public class A1DynamicProgramming {
     }
 
     public static long bDP(int n, long[] a, long[] b){
-        if(n ==0) return 0;
-        if(n ==1) return 1;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
         if(b[n] == -1){
             b[n] = bDP(n - 2, a, b) + aDP(n - 1, a, b);
         }
@@ -42,8 +42,31 @@ public class A1DynamicProgramming {
         return b[n];
     }
 
+    private static long aIter(int n) {
+        long[] a = new long[n + 1];
+        long[] b = new long[n + 1];
+
+        a[0] = 1;
+        b[0] = 0;
+        if(n == 0){
+            return 1;
+        }
+        a[1] = 0;
+        b[1] = 1;
+        if(n == 1){
+            return 0;
+        }
+
+        for(int i = 2; i <= n; i++){
+            b[i] = a[i - 1] + b[i - 2];
+            a[i] = a[i - 2] + 2 * b[i - 1];
+        }
+        return a[n];
+    }
+
     public static void main(String args[]){
         System.out.println(aDP(54));
+        System.out.println(aIter(54));
     }
 
 }
